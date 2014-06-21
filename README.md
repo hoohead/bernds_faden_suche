721b144cbce36a06f4ef62f82483e6b67b4a61ac
hoohead <hoohost@googlemail.com>
2014-06-21 17:47:38 +0200

    Bernds Fadensuche - lauern ohne zu lauern

---
+++ README.md
@@ -0,0 +1,5 @@
+Bernds Fadensuche - lauern ohne zu lauern.
+Es wird immer der komplette Faden durchsucht, nicht nur der Text auf Seite 1.
+Deshalb kann es ein wenig dauern, bis alle 140 Faden durchsucht sind.
+
+[hoohead](http://hoohead.hoohost.org/)
---
+++ fadensuche.php
@@ -0,0 +1,38 @@
+<?php
+$faden = array();
+$forum = $argv[1];
+$suchenach = $argv[2];
+
+for($y=0;$y<14;$y++) {
+    seitensuche("http://krautchan.net/".$forum."/".$y.".html");
+}
+
+echo count($faden)." Faden geladen, starte Fadendurchsuchung ...\n";
+for ($z=0;$z<count($faden);$z++) {
+    echo "Faden: ".($z+1)."\n";
+    suche_im_faden("http://krautchan.net".$faden[$z], $suchenach);
+}
+
+
+
+function seitensuche($forenseite) {
+    global $faden;
+    $source = file_get_contents($forenseite);
+    preg_match_all ('/(<a href=\")(.*?)(\">Antworten<)/i',  $source, $matches);
+    for ($i=0;$i<count($matches[2]);$i++) {
+        $faden[] = $matches[2][$i];   
+    }
+   
+}
+
+function suche_im_faden($fadenurl, $suchenach) {
+    $source = file_get_contents($fadenurl);
+    if(preg_match("/".strtolower($suchenach)."/",strtolower($source))) {
+        echo "gefunden -> ".$fadenurl."\n";
+    }
+}
+
+//Nutze php fadensuche.php ForumID Suchstring
+//php fadensuche b "Axel Stoll"
+
+?>
